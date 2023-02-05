483d5b54f04f240486a34fc692b7afbdf3f75029
Artur <82472015+benyaar@users.noreply.github.com>
2023-02-05 23:33:51 +0200

    feat: init project, connect to db

---
+++ back/src/index.ts
@@ -0,0 +1,24 @@
+import express, {Request, Response} from 'express'
+import * as dotenv from 'dotenv'
+import {sequelize} from "./db";
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get('/blog', (req:Request, res:Response) => {
+    res.send('hello')
+})
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => {console.log(`server start on port ${PORT}`)})
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+start()
