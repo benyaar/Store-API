d84876f85a1a8a767941ab6f7315c24093a0243c
Artur <82472015+benyaar@users.noreply.github.com>
2023-02-06 23:50:11 +0200

    feat: create and find endpoints for brand

--- src/controllers/brandController.ts
+++ src/controllers/brandController.ts
@@ -1,12 +1,17 @@
 import {Request, Response} from "express";
+import {Brand} from "../models/models";
 
 class BrandController {
     async create(req: Request, res:Response){
+        const {name} = req.body
+        const type = await Brand.create({name})
+        return res.status(201).send(type)
 
     }
 
     async getAll(req: Request, res:Response){
-
+        const types = await Brand.findAll()
+        return res.status(200).send(types)
     }
 }
 
