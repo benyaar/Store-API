4c9103f816195e210d995ae5b8d646049371ec94
Artur <82472015+benyaar@users.noreply.github.com>
2023-02-06 23:51:51 +0200

    fit: name variables in brandController.ts

--- src/controllers/brandController.ts
+++ src/controllers/brandController.ts
@@ -4,14 +4,14 @@ import {Brand} from "../models/models";
 class BrandController {
     async create(req: Request, res:Response){
         const {name} = req.body
-        const type = await Brand.create({name})
-        return res.status(201).send(type)
+        const brand = await Brand.create({name})
+        return res.status(201).send(brand)
 
     }
 
     async getAll(req: Request, res:Response){
-        const types = await Brand.findAll()
-        return res.status(200).send(types)
+        const brands = await Brand.findAll()
+        return res.status(200).send(brands)
     }
 }
 
