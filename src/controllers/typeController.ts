ddac99ea30fcf57ec48765f79be1ed04cd7e8d24
Artur <82472015+benyaar@users.noreply.github.com>
2023-02-06 23:50:16 +0200

    feat: create and find endpoints for type

--- src/controllers/typeController.ts
+++ src/controllers/typeController.ts
@@ -1,12 +1,17 @@
 import {Request, Response} from "express";
+import {Type} from "../models/models";
 
 class TypeController {
     async create(req: Request, res:Response){
+        const {name} = req.body
+        const type = await Type.create({name})
+        return res.status(201).send(type)
 
     }
 
     async getAll(req: Request, res:Response){
-
+        const types = await Type.findAll()
+        return res.status(200).send(types)
     }
 }
 
